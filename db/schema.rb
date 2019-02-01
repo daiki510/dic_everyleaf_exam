9c7011cf54d9ec4e06ef45ca15ffb9eb9fdddd3a
ddgg7755 <ddgg7755@gmail.com>
2019-02-01 17:54:57 +0900

    statusカラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_01_074244) do
+ActiveRecord::Schema.define(version: 2019_02_01_085255) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_02_01_074244) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.date "deadline", default: "2019-02-01", null: false
+    t.string "status", default: "未着手", null: false
   end
 
 end
