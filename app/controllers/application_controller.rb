279b23d130e9443147c42608658c45a4506ad102
ddgg7755 <ddgg7755@gmail.com>
2019-02-01 16:21:20 +0900

    herokuでのBasic認証の追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  #Basic認証の追加　
+  http_basic_authenticate_with :name => ENV['BASIC_USERNAME'], :password => ENV['BASIC_PASSWORD'] if Rails.env == "production"
+
 end
