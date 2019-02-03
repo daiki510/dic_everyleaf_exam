c59b2507e060b00abdd416d63e61367a5c8de358
ddgg7755 <ddgg7755@gmail.com>
2019-02-03 21:38:04 +0900

    titleカラムにインデックス追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,12 +3,4 @@ class Task < ApplicationRecord
   validates :content, presence: true, length: {maximum: 150}
   validates :deadline, presence: true
   validates :status, presence: true
-
-  # def self.search(title)
-  #   if title
-  #     Task.where(['title LIKE ?', "%#{title}%"])
-  #   else
-  #     Task.all
-  #   end
-  # end
 end
