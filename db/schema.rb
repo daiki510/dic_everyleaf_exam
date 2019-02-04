5fbe5267ff3cd97225daaa61bb2b64a804b83a64
ddgg7755 <ddgg7755@gmail.com>
2019-02-04 12:10:48 +0900

    優先順位のカラム:priorityの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_03_123301) do
+ActiveRecord::Schema.define(version: 2019_02_04_030852) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_02_03_123301) do
     t.datetime "updated_at", null: false
     t.string "status", default: "未着手", null: false
     t.date "deadline", default: "2019-02-03", null: false
+    t.integer "priority", default: 0, null: false
     t.index ["title"], name: "index_tasks_on_title"
   end
 
