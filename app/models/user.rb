c2281dc78f2d982ac69515962392f1af366111d6
ddgg7755 <ddgg7755@gmail.com>
2019-02-05 15:39:02 +0900

    userモデルへバリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: {maximum: 30}
+  validates :email, presence: true, length: {maximum: 100},format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },uniqueness: true
+  validates :password, presence: true, length: {minimum: 6}, on: :create
+
+  #メールアドレスを全て小文字へ変換
+  before_validation { email.downcase! }
+
+  #パスワードをハッシュ化
+  has_secure_password
 end
