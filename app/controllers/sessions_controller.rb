488f24dacc79ccc90b29ec5cb22581592f09d688
ddgg7755 <ddgg7755@gmail.com>
2019-02-05 17:14:04 +0900

    ログアウト機能の追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,14 @@ class SessionsController < ApplicationController
     
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      redirect_to user_path(user.id)
+      redirect_to user_path(user.id), notice: "ログインしました"
     else
-      flash[:danger] = "ログインに失敗しました"
-      render "new"
+      render "new", notice: "ログインに失敗しました"
     end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    redirect_to new_session_path, notice: "ログアウトしました"
   end
 end
