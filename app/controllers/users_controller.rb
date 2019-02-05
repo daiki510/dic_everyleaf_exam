cdb951b409daf0a1816c6450be68ee9c987a2b8c
ddgg7755 <ddgg7755@gmail.com>
2019-02-05 16:23:07 +0900

    ユーザー登録機能追加＆詳細画面の作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,12 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user.id), notice: "ユーザー登録が完了しました"
+    else
+      render "new"
+    end
   end
   
   def show
