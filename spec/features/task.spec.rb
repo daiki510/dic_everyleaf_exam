3df3e6324d8cd9d354a2ad89fc851485ed024b00
ddgg7755 <ddgg7755@gmail.com>
2019-02-07 15:35:38 +0900

    タスクの新規作成テストを追記

--- spec/features/task.spec.rb
+++ spec/features/task.spec.rb
@@ -15,22 +15,24 @@ RSpec.feature "タスク管理機能", type: :feature do
     fill_in 'Password',	with: login_user.password
     click_button 'Log in'
   end
+  
+  #表示されるタスクの共通化
+  shared_examples_for  "ユーザー1が作成したタスクが表示される" do
+    it { expect(page).to have_content 'testtesttest' } 
+  end
+  
 
   feature '一覧表示機能' do
     context "ユーザー1がログインしている時" do
       let(:login_user) { user_01 } 
 
-      scenario "ユーザー1が作成したタスクが表示される" do
-        #作成済みのタスクの名称が画面上に表示されていることを確認
-        save_and_open_page
-        expect(page).to have_content 'testtesttest'
-      end
+      it_behaves_like "ユーザー1が作成したタスクが表示される"
     end
 
     context "ユーザー2がログインしている時" do
       let(:login_user) { user_02 } 
 
-      scenario "ユーザー1が作成したタスクが表示されない" do
+      it "ユーザー1が作成したタスクが表示されない" do
         #作成済みのタスクの名称が画面上に表示されないことを確認
         expect(page).not_to have_content 'testtesttest'
       end
@@ -44,17 +46,45 @@ RSpec.feature "タスク管理機能", type: :feature do
       before do
         #詳細画面に移動
         visit task_path(task_01)
-        save_and_open_page
       end
 
-      scenario "ユーザー1が作成したタスクが表示される" do
-        save_and_open_page
-        expect(page).to have_content 'testtesttest'
+      it_behaves_like "ユーザー1が作成したタスクが表示される"
+    end
+  end
+
+  feature '新規作成機能' do
+    let(:login_user) { user_01 } 
+
+    before do 
+      visit new_task_path
+      fill_in "タイトル", with: task_name
+      fill_in "内容", with: "新規作成の内容が書かれています"
+      click_button "登録する"
+    end
+
+    context "新規作成画面で名称を入力した時" do
+      let(:task_name) { "新規作成のテスト" } 
+
+      it "正常に登録される" do 
+        expect(page).to have_selector ".notice", text: "タスク「新規作成のテスト」を登録しました"
       end
     end
+    
+    context "新規作成画面で名称を入力しなかった時" do
+      let(:task_name) { "" } 
+      
+      it "エラーとなる" do
+        within "#error_explanation" do
+        save_and_open_page
+          expect(page).to have_content "タイトルを入力してください"
+        end
+      end
+    end
+    
   end
 end
 
+# save_and_open_page
 
 #   #タスク一覧のテスト
 #   scenario "タスク一覧画面に遷移したら、作成済みのタスクが表示される" do
