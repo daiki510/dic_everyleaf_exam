383ff356ad5fc7abc6917b25c54817ba623c00de
ddgg7755 <ddgg7755@gmail.com>
2019-02-08 12:31:42 +0900

    管理者からのユーザー登録機能の追加

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,19 +2,49 @@ class Admin::UsersController < ApplicationController
   before_action :require_admin
 
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # session[:user_id] = @user.id
+      redirect_to admin_users_path(@user.id), notice: "ユーザー登録が完了しました"
+    else
+      render "new"
+    end
+  end
+
+  def index
+
+  end
+
+  
+  def show
+    
   end
 
   def edit
   end
 
-  def show
+  def update
+
   end
 
-  def index
+  def destroy
+
   end
 
   private
+  def set_user
+    @user = User.find(params[:id])
+  end
 
+  def user_params
+    params.require(:user).permit(:name,:email,:password,:password_confirmation, :admin)
+  end
+
+  #管理者のみユーザー管理画面へアクセスできるように制御
   def require_admin
     unless current_user.admin? 
      redirect_to root_path , notice: "権限がありません"
