d66051a569cbbbc70d87381c5753c1ca87bc8403
ddgg7755 <ddgg7755@gmail.com>
2019-02-09 15:18:34 +0900

    管理者ユーザーのテストコードをリファクタ

--- spec/features/admin_user.spec.rb
+++ spec/features/admin_user.spec.rb
@@ -1,22 +1,21 @@
 require 'rails_helper'
 
 RSpec.feature "管理者によるユーザー管理機能", type: :feature do
+  let(:admin_user_01){FactoryBot.create(:admin_user)}
+  let(:user_01){FactoryBot.create(:user)}
+
+  before do 
+    visit new_session_path
+    fill_in 'Email', with: login_user.email
+    fill_in 'Password',	with: login_user.password
+    click_button 'Log in'
+  end
 
   feature 'ユーザー一覧機能' do
-    let(:admin_user_01){FactoryBot.create(:admin_user)}
-    let(:user_01){FactoryBot.create(:user)}
-
-    before do 
-      visit new_session_path
-      fill_in 'Email', with: login_user.email
-      fill_in 'Password',	with: login_user.password
-      click_button 'Log in'
-    end
-
     context "管理者がログインしている時" do
       let(:login_user) { admin_user_01 }
       it "ユーザー管理が表示される" do
-        save_and_open_page
+        # save_and_open_page
         expect(page).to have_content 'ユーザー管理'
       end
     end
@@ -30,17 +29,9 @@ RSpec.feature "管理者によるユーザー管理機能", type: :feature do
   end
     
   feature '新規登録機能' do
+    context "登録時に管理者権限にチェックを入れる" do
+      let(:login_user) { admin_user_01 }
 
-    before do 
-      #管理者ユーザーでログインする
-      FactoryBot.create(:admin_user)
-      visit new_session_path
-      fill_in 'Email', with: "admin@gmail.com"
-      fill_in 'Password',	with: "000000"
-      click_button 'Log in'
-    end
-
-    context "管理者権限にチェックを入れる" do
       it "新規管理者ユーザーは、ユーザー管理画面へアクセスできる" do
         visit new_admin_user_path
       
@@ -66,48 +57,37 @@ RSpec.feature "管理者によるユーザー管理機能", type: :feature do
   end
 
   feature '編集機能' do
-    before do 
-      #管理者ユーザーでログインする
-      FactoryBot.create(:admin_user)
-      @user = FactoryBot.create(:user) 
-      visit new_session_path
-      fill_in 'Email', with: "admin@gmail.com"
-      fill_in 'Password',	with: "000000"
-      click_button 'Log in'
-      click_link 'ユーザー管理'
-    end
+    let(:login_user) { admin_user_01 }
     
     context "管理者が一般ユーザーの編集画面へ" do
+      before do
+        @user = FactoryBot.create(:user, name: "testuser_before")
+        click_link 'ユーザー管理'
+      end
       it "名前を編集すると変更されているかどうか" do
         visit edit_admin_user_path(@user.id)
 
-        fill_in "名前",	with: "testuser_update" 
+        fill_in "名前",	with: "testuser_after" 
         click_button '更新する'
+        save_and_open_page
         
-        expect(page).to have_content 'testuser_update'
+        expect(page).to have_content 'testuser_after'
       end
     end
   end
 
   feature '削除機能' do
-    before do 
-      #管理者ユーザーでログインする
-      FactoryBot.create(:admin_user)
-      @user = FactoryBot.create(:user) 
-      visit new_session_path
-      fill_in 'Email', with: "admin@gmail.com"
-      fill_in 'Password',	with: "000000"
-      click_button 'Log in'
-      click_link 'ユーザー管理'
-    end
-
+    let(:login_user) { admin_user_01 }
+    
     context "管理者がユーザー管理画面へ" do
+      before do
+        FactoryBot.create(:user, email: "testuser01@gmail.com")
+        click_link 'ユーザー管理'
+      end
       it "削除ボタンを押した時に削除されているかどうか" do
-       
         find('.delete_link').click
 
-        # save_and_open_page
-        expect(page).not_to have_content 'testuser@gmail.com'
+        expect(page).not_to have_content 'testuser01@gmail.com'
       end
     end
   end
