8d6df4d41beaf98a974c6c8ba330fe74f8809b34
ddgg7755 <ddgg7755@gmail.com>
2019-04-10 10:34:54 +0900

    コーディング規約にに沿って修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
   before_action :set_user, only: [:show]
   before_action :authenticate_user, only: [:show]
-  before_action :forbid_login_user, only: [:new, :create]
+  before_action :forbid_login_user, only: %i[new create]
   before_action :enssure_correct_user, only: [:show]
-
 
   def new
     @user = User.new
@@ -13,15 +14,13 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to user_path(@user.id), notice: "ユーザー登録が完了しました"
+      redirect_to user_path(@user.id), notice: 'ユーザー登録が完了しました'
     else
-      render "new"
+      render 'new'
     end
   end
-  
-  def show
-    
-  end
+
+  def show; end
 
   private
 
@@ -30,6 +29,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:name,:email,:password,:password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
